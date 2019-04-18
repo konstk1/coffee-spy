bf6ee35d0c6ada241c594536c8b4d0bdf639a7b5
Konstantin Klitenik <kostyan5@gmail.com>
2019-04-18 16:55:21 -0400

    Add startup banner

--- src/main.cpp
+++ src/main.cpp
@@ -16,31 +16,21 @@
 
 #include "Logger.h"
 
-static const Logger log = Logger("MAIN");
+static const Logger log("MAIN");
 
 extern "C" {
 
 void app_main() {
-    std::cout << "Hello world!" << std::endl;
+    esp_chip_info_t chipInfo;
+    esp_chip_info(&chipInfo);
 
-    log.Error("Error test log %d", 5);
-    log.Warn("Warn test log %s", "warning");
-    log.Info("Info test log %f", 5.3);
-    log.Verbose("Verb test log %x", 0xface);
-
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    std::cout << "========================================" << std::endl;
+    std::cout << "* Welcome to Coffee-Spy" << std::endl;
+    std::cout << "*" << std::endl;
+    std::cout << "* ESP32 Chip Rev: " << static_cast<int>(chipInfo.revision) << " (" << static_cast<int>(chipInfo.cores) << " cores)" << std::endl;
+    std::cout << "* ESP32 IDF Ver:  " << esp_get_idf_version() << std::endl;
+    std::cout << "* Flash size:     " << spi_flash_get_chip_size() / 1048576 << " MB" << std::endl;
+    std::cout << "========================================" << std::endl;
 
     // for (int i = 10; i >= 0; i--) {
     //     printf("Restarting in %d seconds...\n", i);
